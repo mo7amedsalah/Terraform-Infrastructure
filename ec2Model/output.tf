2195e2b5bf7f43d2a4d52836e3e38f2501953949
mo7amedsalah <mohamed.s.abokhadra@gmail.com>
2020-04-05 23:14:05 +0200

    destroy

--- ec2Model/output.tf
+++ ec2Model/output.tf
@@ -1,8 +1,8 @@
 output "public_key" {
-  value = "${tls_private_key.keygen1-ssh.public_key_pem}"
+  value = "${tls_private_key.ssh_key.public_key_pem}"
   description = "The TLS genreated public key value"
 }
 output "private_key" {
-  value = "${tls_private_key.keygen1-ssh.private_key_pem}"
+  value = "${tls_private_key.ssh_key.private_key_pem}"
   description = "The TLS genreated private key value"
 }
