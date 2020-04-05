93532ee90bdf171eff77346a6c68e383bec6eb84
mo7amedsalah <mohamed.s.abokhadra@gmail.com>
2020-04-05 23:44:05 +0200

    destroy

--- ec2Model/output.tf
+++ ec2Model/output.tf
@@ -1,3 +1,10 @@
-output "public_ip" {
-  value = "${aws_instance.app.public_ip}"
+
+output "public_key" {
+  value = "${tls_private_key.ssh_key.public_key_pem}"
+  description = "The TLS genreated public key value"
+}
+
+output "private_key" {
+  value = "${tls_private_key.ssh_key.private_key_pem}"
+  description = "The TLS genreated private key value"
 }
