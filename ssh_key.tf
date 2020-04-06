18e1c78e55ff0e86734ce710cc9e528fb8d59632
mo7amedsalah <mohamed.s.abokhadra@gmail.com>
2020-04-06 23:01:12 +0200

    delete destroy

--- ssh_key.tf
+++ ssh_key.tf
@@ -23,12 +23,4 @@ provisioner "local-exec" {
   interpreter = ["bash", "-c"]
   }
 }
-output "public_key" {
-  value = "${tls_private_key.ssh_key.public_key_pem}"
-  description = "The TLS genreated public key value"
-}
 
-output "private_key" {
-  value = "${tls_private_key.ssh_key.private_key_pem}"
-  description = "The TLS genreated private key value"
-}
